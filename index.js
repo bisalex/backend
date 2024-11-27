e9e235bfb265599ff898b4d276d60788dda2277e
Yisihak Kassa <yisihakassa@gmail.com>
2024-11-27 01:07:15 +0100

    added express static

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 
+
 app.use(cors())
 
 app.use(express.json())
+app.use(express.static('dist'))
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
   console.log('Path:  ', request.path)
